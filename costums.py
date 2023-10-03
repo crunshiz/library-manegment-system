1715a25284fb306f7b64babed54d57ca60b73416
crunshiz <106341918+crunshiz@users.noreply.github.com>
2023-10-04 02:16:32 +0400

    Create costums.py

---
+++ costums.py
@@ -0,0 +1,9 @@
+gray = "gray95"
+black = "black"
+white = "white"
+
+tnr = "times new roman"
+helvetica = "helvetica"
+
+columns_1 = ('book_id', 'book_name', 'student_roll', 'student_name', 'course', 'subject', 'issue_date', 'return_date')
+columns_2 = ('book_id', 'book_name', 'author', 'edition', 'price', 'qty')
